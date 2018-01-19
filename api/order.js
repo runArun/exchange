3a5d64328d700ccf1a59c7ff028bb06102a9ff73
runArun <zl2613185@qq.com>
2018-01-19 22:54:48 +1100

    list orders

--- api/order.js
+++ api/order.js
@@ -1,11 +1,11 @@
-import orders from '../models/order';
+import Orders from '../models/order';
 import { Router } from 'express';
 
 let order = Router();
 
 order.post('/', function(req, res, next){
 
-    let order = new orders({
+    let order = new Orders({
         username: req.body.username,
         holdC: req.body.holdC,
         wantC: req.body.wantC,
@@ -16,6 +16,17 @@ order.post('/', function(req, res, next){
     console.log(order);
     order.save();
     res.sendStatus(200);
+});
+
+order.get('/', function (req, res, next) {
+    Orders.find(function (err, data) {
+        if (err){
+            console.log(err);
+        }
+        else {
+            res.send(data);
+        }
+    })
 });
 
 export default order;
