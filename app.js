e339031da3428a964f25e1e2b0f17d9a26859bfd
runArun <zl2613185@qq.com>
2018-01-20 21:53:11 +1100

    git commit

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var path = require('path');
+import express from 'express';
+import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import api from './api';
-
 
 var app = express();
 
@@ -11,8 +10,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use('/api', api);
 
 module.exports = app;
